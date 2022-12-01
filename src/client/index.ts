6b06847f551a4e8174ed3d219ec3630e4c016a9a
Severin Buhler <sevi_buehler@hotmail.com>
2022-12-01 09:26:36 +0200

    enabled vscode debug options + added example server + client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,25 @@
+const Link = require('grenache-nodejs-link'); // No types available? :(
+    const { PeerRPCClient } = require('grenache-nodejs-http'); // No types available? :(
+
+function exampleClient(){
+    const link = new Link({
+    grape: 'http://127.0.0.1:30001'
+    })
+    link.start()
+
+    const peer = new PeerRPCClient(link, {})
+    peer.init()
+
+    peer.request('rpc_test', 'hello', { timeout: 10000 }, (err: any, data: any) => {
+    if (err) {
+        console.error(err)
+        process.exit(-1)
+    }
+    console.log(data) // world
+    })
+}
+
 export function startClient() {
     console.log('Start client.');
+    exampleClient();
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,32 @@
+const { PeerRPCServer } = require('grenache-nodejs-http'); // No types available? :(
+const Link = require('grenache-nodejs-link');
+
+function exampleServer() {
+    const link = new Link({
+        grape: 'http://127.0.0.1:30001'
+    })
+    link.start()
+
+    const peer = new PeerRPCServer(link, {
+        timeout: 300000
+    })
+    peer.init()
+
+    const port = 1024 + Math.floor(Math.random() * 1000)
+    const service = peer.transport('server')
+    service.listen(port)
+
+    setInterval(() => {
+        link.announce('rpc_test', service.port, {})
+    }, 1000);
+
+    service.on('request', (rid: any, key: any, payload: any, handler: any) => {
+        console.log(payload) //  { msg: 'hello' }
+        handler.reply(null, { msg: 'world' })
+    })
+}
+
 export function startServer() {
     console.log('Start server.');
+    exampleServer();
 }
