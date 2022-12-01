5eb9793c95c1606f0c523a89c4af4f6a4ee79ceb
Severin Buhler <sevi_buehler@hotmail.com>
2022-12-01 10:09:03 +0200

    added ids to clients and servers

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,25 +1,34 @@
 const Link = require('grenache-nodejs-link'); // No types available? :(
-    const { PeerRPCClient } = require('grenache-nodejs-http'); // No types available? :(
+const { PeerRPCClient } = require('grenache-nodejs-http'); // No types available? :(
 
-function exampleClient(){
+function exampleClient(clientId: number){
+
+
     const link = new Link({
     grape: 'http://127.0.0.1:30001'
-    })
-    link.start()
+    });
+    link.start();
 
-    const peer = new PeerRPCClient(link, {})
-    peer.init()
+    const peer = new PeerRPCClient(link, {});
+    peer.init();
 
-    peer.request('rpc_test', 'hello', { timeout: 10000 }, (err: any, data: any) => {
-    if (err) {
-        console.error(err)
-        process.exit(-1)
+    const msg = JSON.stringify({
+        msg: 'hello',
+        clientId: clientId
+    });
+    for (let i = 0; i< 10; i++) {
+        peer.request('rpc_test', msg, { timeout: 10000 }, (err: any, data: any) => {
+            if (err) {
+                console.error(err);
+                process.exit(-1);
+            }
+            console.log(i, data); // world
+        });
     }
-    console.log(data) // world
-    })
 }
 
 export function startClient() {
-    console.log('Start client.');
-    exampleClient();
+    const clientId = Math.floor(Math.random() * 9999);
+    console.log('Start client', clientId);
+    exampleClient(clientId);
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,32 +1,34 @@
 const { PeerRPCServer } = require('grenache-nodejs-http'); // No types available? :(
 const Link = require('grenache-nodejs-link');
 
-function exampleServer() {
+function exampleServer(serverId: number) {
     const link = new Link({
         grape: 'http://127.0.0.1:30001'
-    })
-    link.start()
+    });
+    link.start();
 
     const peer = new PeerRPCServer(link, {
         timeout: 300000
-    })
-    peer.init()
+    });
+    peer.init();
 
-    const port = 1024 + Math.floor(Math.random() * 1000)
-    const service = peer.transport('server')
-    service.listen(port)
+    const port = 1024 + Math.floor(Math.random() * 1000);
+    const service = peer.transport('server');
+    service.listen(port);
+    console.log('Listening on port', port);
 
     setInterval(() => {
         link.announce('rpc_test', service.port, {})
     }, 1000);
 
     service.on('request', (rid: any, key: any, payload: any, handler: any) => {
-        console.log(payload) //  { msg: 'hello' }
-        handler.reply(null, { msg: 'world' })
+        console.log(serverId, payload); //  { msg: 'hello' }
+        handler.reply(null, { msg: 'world', serverId: serverId });
     })
 }
 
 export function startServer() {
-    console.log('Start server.');
-    exampleServer();
+    const serverId = Math.floor(Math.random() * 9999);
+    console.log('Start server', serverId);
+    exampleServer(serverId);
 }
